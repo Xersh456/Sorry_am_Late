a20469c8e23bf5392312b7c96a60806d32291365
Xersh456 <85128466+Xersh456@users.noreply.github.com>
2021-06-05 18:42:03 +0700

    Add files via upload

---
+++ room_ros_description/urdf/UnfinishKeyboard.py
@@ -0,0 +1,13 @@
+def main():
+    if getkey('w'):
+
+    elif getkey('a'):
+
+    elif getkey('s'):
+
+    elif getkey('d'):
+
+if __name__ == '__main__'
+    init()
+    while True:
+        main()
